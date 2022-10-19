cc4e05020e971d6770ff4f7880c1e5b9b60649d7
Erik Slovák <34162967+erik-slovak@users.noreply.github.com>
2022-10-19 20:05:56 +0200

    fix: initial implementation (#1)
    * chore: add empty package
    
    * feat: add initial implementation
    
    * style: add missing ending line
    
    Co-authored-by: Rostislav Simonik <rostislav.simonik@technologystudio.sk>

---
+++ src/Model/Types.ts
@@ -0,0 +1,8 @@
+/**
+ * @Author: Erik Slovak <erik.slovak@technologystudio.sk>
+ * @Date: 2022-10-19T09:10:47+02:00
+ * @Copyright: Technology Studio
+**/
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface DefaultRootState {}
